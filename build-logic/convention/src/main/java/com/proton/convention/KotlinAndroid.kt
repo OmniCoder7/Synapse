8c4f9517aa4039a43ad41afec758dbd0214caa7f
ProtonDeveloper77 <rishabhsaraswat17@gmail.com>
2024-09-14 21:43:12 +0530

    Initial Commit

--- build-logic/convention/src/main/java/com/proton/convention/KotlinAndroid.kt
+++ build-logic/convention/src/main/java/com/proton/convention/KotlinAndroid.kt
@@ -64,7 +64,7 @@ private inline fun <reified T : KotlinTopLevelExtension> Project.configureKotlin
     when (this) {
         is KotlinAndroidProjectExtension -> compilerOptions
         is KotlinJvmProjectExtension -> compilerOptions
-        else -> TODO("Unsupported project extension $this ${T::class}")
+        else ->
     }.apply {
         jvmTarget = JvmTarget.JVM_11
         allWarningsAsErrors = warningsAsErrors.toBoolean()
